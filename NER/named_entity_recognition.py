c329f70a8e45472382c686a620ce14a5a20f96d1
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2024-12-09 16:16:15 +0100

    feat: Create NER class

--- NER/named_entity_recognition.py
+++ NER/named_entity_recognition.py
@@ -1,7 +1,32 @@
 import spacy
 
+
+class NamedEntityRecognition:
+    def __init__(self):
+        self.nlp = spacy.load("en_core_web_sm")
+
+    def get_named_entities(self, text:str):
+        doc = self.nlp(text)
+        named_entities = []
+        for ent in doc.ents:
+            named_entities.append((ent.text, ent.label_))
+        return named_entities
+
+    def get_named_entities_from_subset(self, text:str, subset:list[str]):
+        """
+        Returns named entities from a subset of named entity types.
+        :param text: Text to analyze as string
+        :param subset: List of named entity categories to consider
+        :return: List of entities and their categories
+        """
+        doc = self.nlp(text)
+        named_entities = []
+        for ent in doc.ents:
+            if ent.label_ in subset:
+                named_entities.append((ent.text, ent.label_))
+        return named_entities
+
 if __name__ == "__main__":
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp("Apple is a big company in the USA.")
-    for ent in doc.ents:
-        print(ent.text, ent.label_)
+    ner = NamedEntityRecognition()
+    print(ner.get_named_entities("Apple is a big company in the USA."))
+    print(ner.get_named_entities_from_subset("Apple is a big company in the USA.", ['ORG']))
