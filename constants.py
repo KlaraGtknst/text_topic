c170659ea3f4f210872e0e74b291e08db03519ed
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2024-12-13 14:39:32 +0100

    feat: Add server path to constants

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 CLIENT_ADDR = "http://localhost:9200"
 DB_NAME = "txt_db"
-TEST_TRAINING_PATH = "/Users/klara/Documents/uni/"#'/Users/klara/Downloads'
+TEST_TRAINING_PATH = '/Users/klara/Downloads'#"/Users/klara/Documents/uni/"#
+SERVER_PATH = '/norgay/bigstore/kgu/data/ETYNTKE'
 SAVE_PATH = '../results'
