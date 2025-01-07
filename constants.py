5f20e4135b25816529a082842b62c6a768390b89
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-07 22:54:46 +0100

    feat: Change path to production path

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 CLIENT_ADDR = "http://localhost:9200"
 DB_NAME = "txt_db"
-TEST_TRAINING_PATH = '/Users/klara/Downloads'#"/Users/klara/Documents/uni/"#
-SERVER_PATH = '/norgay/bigstore/kgu/data/ETYNTKE/Physics'
+TEST_TRAINING_PATH = '/Users/klara/Downloads'   #"/Users/klara/Documents/uni/"#
+SERVER_PATH = '/norgay/bigstore/kgu/data/ETYNTKE'   #/Physics
 SERVER_PATH_TO_PROJECT = '/norgay/bigstore/kgu/dev/text_topic/'
 SAVE_PATH = '../results'
