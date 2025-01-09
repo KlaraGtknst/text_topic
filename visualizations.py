8d78615c93460943f01f4042f5bd6fa54d93b513
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-09 15:47:02 +0100

    feat: Try using TSNE

--- visualizations.py
+++ visualizations.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
 
     # 2D scatter plot of the documents colored by their parent directory
     client = Elasticsearch(constants.CLIENT_ADDR)
-    scatter_documents_2d(client, save_path=constants.SERVER_SAVE_PATH)
+    scatter_documents_2d(client, save_path=constants.SERVER_SAVE_PATH, use_tsne=True)
