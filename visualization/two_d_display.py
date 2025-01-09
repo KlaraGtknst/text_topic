4044dde5ee1904908405719827a7080d2dfdd9f5
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-09 16:01:52 +0100

    feat: Add option of processing of dir names & unique id

--- visualization/two_d_display.py
+++ visualization/two_d_display.py
@@ -7,9 +7,48 @@ import pandas as pd
 from utils.os_manipulation import save_or_not
 from sklearn.manifold import TSNE
 import numpy as np
+import re
+
+def process_directory_names(dir_list:list):
+    """
+    Process directory names to remove numbers and replace similar names with a common base name.
+    :param dir_list: A list of directory names.
+    :return: A list of processed directory names, where similar names are replaced with a common base name and
+    words with >50% numbers are replaced with the word 'numbers'.
+    """
+    def is_mostly_numbers(s):
+        """Check if a string has more than 50% digits."""
+        num_count = sum(c.isdigit() for c in s)
+        return num_count > len(s) / 2
+
+    def remove_numbers(s):
+        """Remove digits from a string."""
+        return re.sub(r'\d+', '', s)
+
+    # Replace strings with >50% numbers with the word 'numbers'
+    processed_dirs = []
+    for dir_name in dir_list:
+        if is_mostly_numbers(dir_name):
+            processed_dirs.append("numbers")
+        else:
+            processed_dirs.append(dir_name)
+
+    # Replace strings differing only by numbers with their shared base word
+    shared_dirs = {}
+    for dir_name in processed_dirs:
+        if dir_name == "numbers":
+            shared_dirs[dir_name] = "numbers"
+            continue
+        base_name = remove_numbers(dir_name)
+        shared_dirs[dir_name] = base_name
+
+    # Generate the final list using shared directory names
+    final_dirs = [shared_dirs[dir_name] for dir_name in processed_dirs]
+    return final_dirs
 
 
-def scatter_documents_2d(client, save_path=None, on_server=False, use_tsne=False):
+def scatter_documents_2d(client, save_path=None, on_server=False, use_tsne=False, preprocess_dirs=True,
+                         unique_id_suffix=""):
     '''
     This function creates a 2D scatter plot of the documents.
     The documents are represented by their embeddings.
@@ -20,6 +59,9 @@ def scatter_documents_2d(client, save_path=None, on_server=False, use_tsne=False
     :param on_server: if the function is run on the server. Since there are deeper directories on the server,
     the documents are coloured according to the uppermost directory.
     :param use_tsne: if True, t-SNE is used for dimensionality reduction, else PCA
+    :param preprocess_dirs: if True, preprocess the directory names, i.e. remove numbers
+    and replace similar names with a common base name
+    :param unique_id_suffix: unique id suffix for the file name; default is empty string (hence, no suffix)
     :return -
     '''
     # obtain results from elastic search
@@ -58,14 +100,18 @@ def scatter_documents_2d(client, save_path=None, on_server=False, use_tsne=False
     embeddings = [r['_source']['embedding'] for r in results]
     class_dirs = [r['_source']['directory'] for r in results]
     # uppermost directory if on server else the directory
-    colour_criteria = [r['_source']['path'].split('/ETYNTKE/')[-1].split('/')[0] for r in results] if on_server else class_dirs
+    colour_criteria = [r['_source']['path'].split('/ETYNTKE/')[-1].split('/')[0] for r in results] \
+        if on_server else class_dirs
+    if preprocess_dirs:
+        colour_criteria = process_directory_names(colour_criteria)
 
     # reduce dimensionality to 2D
+    transformation = "TSNE" if use_tsne else "PCA"
     if use_tsne:
         tsne = TSNE(n_components=2)
         transformed_embs = tsne.fit_transform(np.array(embeddings))
     else:
-        pca = PCA(n_components=2)  # TODO: TSNE/ UMAP instead of PCA?
+        pca = PCA(n_components=2)  # TODO: UMAP
         transformed_embs = pca.fit_transform(embeddings)
 
     # create dataframe
@@ -77,7 +123,8 @@ def scatter_documents_2d(client, save_path=None, on_server=False, use_tsne=False
     plt.title('2D scatter plot of the documents')
     plt.legend(loc="upper left", fontsize="9", fancybox=True, shadow=True, ncol=3, bbox_to_anchor=(1.04, 1))
     fig.tight_layout()  # if too many classes: will rise a warning, but the plot will be saved correctly
-    save_or_not(plt, file_name='scatter_documents_dir_2d.png', save_path=save_path, format='svg')
+    save_or_not(plt, file_name=f'{transformation}_scatter_documents_dir_2d_{unique_id_suffix}.png',
+                save_path=save_path, format='svg')
     plt.show()
 
 # if __name__ == '__main__':
--- visualizations.py
+++ visualizations.py
@@ -1,9 +1,12 @@
 from elasticsearch import Elasticsearch
 from visualization.two_d_display import scatter_documents_2d
 import constants
+import datetime
 
 if __name__ == '__main__':
+    date = datetime.datetime.now().strftime('%x').replace('/', '_')
 
     # 2D scatter plot of the documents colored by their parent directory
     client = Elasticsearch(constants.CLIENT_ADDR)
-    scatter_documents_2d(client, save_path=constants.SERVER_SAVE_PATH, use_tsne=True)
+    scatter_documents_2d(client, save_path=constants.SERVER_SAVE_PATH, use_tsne=True, unique_id_suffix=date)
+    scatter_documents_2d(client, save_path=constants.SERVER_SAVE_PATH, use_tsne=False, unique_id_suffix=date)
