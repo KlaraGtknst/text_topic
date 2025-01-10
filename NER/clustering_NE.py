e38d8e5cf4f892546f99f59c76edca14cf02ff1d
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-10 16:57:40 +0100

    style: Improve commentary & docstrings

--- NER/clustering_NE.py
+++ NER/clustering_NE.py
@@ -9,12 +9,25 @@ import json
 from collections import Counter, defaultdict
 
 import constants
+from utils.os_manipulation import exists_or_create
 
 
 class ClusterNamedEntities:
 
     def __init__(self, client, index=constants.DB_NAME, category='ORG', top_n=50, n_clusters=5, output_file="",
                  es_request_limit=10000):
+        """
+        Initialize the Named Entity Clustering class.
+        :param client: Elasticsearch client, already connected to the server
+        :param index: Name of the Elasticsearch index
+        :param category: String representing the named entity category to cluster
+        :param top_n: Number of top named entities to consider for clustering;
+            top in terms of frequency of occurrence across documents
+        :param n_clusters: Number of clusters to form
+        :param output_file: The path to save the clustering results including the file name and extension.
+            If not provided, the results are saved in the server's save path with a default name.
+        :param es_request_limit: Number of documents to fetch in each Elasticsearch request at a time.
+        """
         self.client = client
         self.index = index
         self.category = category
@@ -68,39 +81,55 @@ class ClusterNamedEntities:
         self.client.clear_scroll(scroll_id=scroll_id)
         return named_entities, doc_map
 
-    def compute_embeddings(self, entities):
+    def compute_embeddings(self, entities:list):
         """
         Encode entities using SBERT model.
+        :param entities: List of named entities to encode
+        :return: List of encoded embeddings
         """
         model = SentenceTransformer('sentence-transformers/msmarco-MiniLM-L-12-v3')
         embeddings = model.encode(entities, convert_to_tensor=False)
         return embeddings
 
-    def get_top_n_entities(self, named_entities):
+    def get_top_n_entities(self, named_entities:list):
         """
         Find the top-N most frequent named entities.
+        :param named_entities: List of named entities
+        :return: List of top-N named entities
         """
         counter = Counter(named_entities)
         top_entities = [entity for entity, _ in counter.most_common(self.top_n)]
         return top_entities
 
-    def calculate_similarity_matrix(self, embeddings):
+    def calculate_similarity_matrix(self, embeddings:list):
         """
         Compute a symmetric similarity matrix for given embeddings.
+        :param embeddings: List of embeddings
+        :return: Symmetric similarity matrix as array
         """
-        return cosine_similarity(embeddings)
+        return cosine_similarity(embeddings)    # pairwise cosine similarity
 
     def cluster_named_entities(self, similarity_matrix):
         """
         Perform k-means clustering on the similarity matrix.
+        :param similarity_matrix: Symmetric similarity matrix as array obtained from the
+            calculate_similarity_matrix(.) method
+        :return: List of cluster assignments for each named entity
         """
         kmeans = KMeans(n_clusters=self.n_clusters, random_state=42)
         clusters = kmeans.fit_predict(similarity_matrix)
         return clusters
 
-    def save_results(self, clusters, top_n_entities, doc_map, top_n_doc_maps, embeddings):
+    def save_results(self, clusters:list, top_n_entities:list, doc_map:dict, top_n_doc_maps:dict, embeddings:list):
         """
-        Save clustering results and entity-document mapping to a JSON file.
+        Save clustering results, embedding and entity-document mapping of the top N named entities to a JSON file.
+        :param clusters: List of cluster assignments for each named entity
+        :param top_n_entities: List of top-N named entities
+        :param doc_map: Mapping of named entities to documents; currently not saved because of large size
+        :param top_n_doc_maps: Mapping of top-N named entities to documents
+            (i.e. for each entity in top-N all documents that contain it)
+        :param embeddings: List of embeddings for top-N named entities
+        :return: None
         """
         result = {
             "category": self.category,
@@ -114,6 +143,7 @@ class ClusterNamedEntities:
                        f"cluster_NE_results_{self.category}_{datetime.datetime.now().strftime('%x').replace('/', '_')}.json") \
             if self.output_file == "" else self.output_file
 
+        exists_or_create(path=constants.SERVER_SAVE_PATH + "/cluster_NER/")
         with open(output_file, "w") as f:
             json.dump(result, f, indent=4)
         print(f"Results saved to {output_file}")
@@ -121,6 +151,8 @@ class ClusterNamedEntities:
     def process_category(self, category=""):
         """
         Perform named entity clustering for a specified category with large datasets.
+        :param category: String representing the named entity category to cluster;
+            if not provided, the default category is used
         """
         if category != "":
             self.category = category
