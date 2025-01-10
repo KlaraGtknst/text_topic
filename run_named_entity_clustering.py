657eb8f7fc474f6ce6893219b62f69670dbe3c6e
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-10 13:43:51 +0100

    feat: Prepare auto clustering for all NE categories

--- run_named_entity_clustering.py
+++ run_named_entity_clustering.py
@@ -1,7 +1,5 @@
-import datetime
-
-import constants
 from NER.clustering_NE import *
+import tqdm
 
 
 if __name__ == '__main__':
@@ -9,7 +7,21 @@ if __name__ == '__main__':
     print('File was run at: ', date)
     client = Elasticsearch(constants.CLIENT_ADDR, request_timeout=60)
     clusterNamedEntities = ClusterNamedEntities(client=client, index=constants.DB_NAME, top_n=50, n_clusters=5)
-    print('--------------------------')
-    clusterNamedEntities.process_category(category="ORG")
-    print('--------------------------')
-    clusterNamedEntities.process_category(category="PERSON")
+
+    # Fetch the index mapping
+    mapping = client.indices.get_mapping(index=constants.DB_NAME)
+    named_entities_mapping = mapping[constants.DB_NAME]["mappings"]["properties"]["named_entities"]["properties"]
+
+    # Extract the keys (categories)
+    categories = list(named_entities_mapping.keys())
+    print("All categories of the nested field 'named_entities': ", categories)
+
+    for i in tqdm.tqdm(range(len(categories)), desc='Obtaining named entities clustering for each category'):
+        category = categories[i]
+        print('--------------------------')
+        clusterNamedEntities.process_category(category=category)
+
+    # print('--------------------------')
+    # clusterNamedEntities.process_category(category="ORG")
+    # print('--------------------------')
+    # clusterNamedEntities.process_category(category="PERSON")
