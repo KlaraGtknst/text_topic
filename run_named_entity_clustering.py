3a9a44cef3df23bf301c613f8b484254e2657460
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-10 14:30:33 +0100

    feat: Use auto generation of all categories of NE

--- run_named_entity_clustering.py
+++ run_named_entity_clustering.py
@@ -10,20 +10,20 @@ if __name__ == '__main__':
     clusterNamedEntities = ClusterNamedEntities(client=client, index=constants.DB_NAME, top_n=top_n,
                                                 n_clusters=top_n // 10)
 
-    # # Fetch the index mapping
-    # mapping = client.indices.get_mapping(index=constants.DB_NAME)
-    # named_entities_mapping = mapping[constants.DB_NAME]["mappings"]["properties"]["named_entities"]["properties"]
-    #
-    # # Extract the keys (categories)
-    # categories = list(named_entities_mapping.keys())
-    # print("All categories of the nested field 'named_entities': ", categories)
-    #
-    # for i in tqdm.tqdm(range(len(categories)), desc='Obtaining named entities clustering for each category'):
-    #     category = categories[i]
-    #     print('--------------------------')
-    #     clusterNamedEntities.process_category(category=category)
+    # Fetch the index mapping
+    mapping = client.indices.get_mapping(index=constants.DB_NAME)
+    named_entities_mapping = mapping[constants.DB_NAME]["mappings"]["properties"]["named_entities"]["properties"]
 
-    print('--------------------------')
-    clusterNamedEntities.process_category(category="ORG")
+    # Extract the keys (categories)
+    categories = list(named_entities_mapping.keys())
+    print("All categories of the nested field 'named_entities': ", categories)
+
+    for i in tqdm.tqdm(range(len(categories)), desc='Obtaining named entities clustering for each category'):
+        category = categories[i]
+        print('--------------------------')
+        clusterNamedEntities.process_category(category=category)
+
+    # print('--------------------------')
+    # clusterNamedEntities.process_category(category="ORG")
     # print('--------------------------')
     # clusterNamedEntities.process_category(category="PERSON")
