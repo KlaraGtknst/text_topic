48be6ccaa9ccff27d467fb4979bfb0a341b5d9fd
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-10 16:46:40 +0100

    style: Improve commentary & docstrings

--- visualization/plotting_utils.py
+++ visualization/plotting_utils.py
@@ -3,7 +3,15 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 import umap
 
-def obtain_low_dim_embs(high_dim_embs, reducer):
+
+def obtain_low_dim_embs(high_dim_embs:list, reducer:str='PCA'):
+    """
+    Obtain low-dimensional embeddings using PCA, t-SNE or UMAP.
+    :param high_dim_embs: High-dimensional embeddings. It should be a list of lists,
+    while the inner lists have the same number of elements.
+    :param reducer: A string specifying the dimensionality reduction technique. One of 'PCA', 'TSNE', 'UMAP'.
+    :return: Transformed embeddings in 2D using the specified dimensionality reduction technique.
+    """
     if reducer == 'TSNE':
         tsne = TSNE(n_components=2)
         transformed_embs = tsne.fit_transform(np.array(high_dim_embs))
