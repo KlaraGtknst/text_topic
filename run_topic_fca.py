76e954ef5defad31e6f752dff92efc2302550d6d
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-14 18:17:31 +0100

    task: Add save date for test

--- run_topic_fca.py
+++ run_topic_fca.py
@@ -5,6 +5,7 @@ from topic.topic_fca import *
 if __name__ == '__main__':
     on_server = True
     date = "01_08_25"
+    save_date = "01_14_25"
     path = constants.SERVER_PATH if on_server else "/Users/klara/Documents/uni/"
     dataset_path = constants.SERVER_PATH_TO_PROJECT + 'dataset/' if on_server else "../dataset/"
     model_path = constants.SERVER_PATH_TO_PROJECT + 'models/' if on_server else '../models/'
@@ -20,14 +21,14 @@ if __name__ == '__main__':
     # Load the doc-topic context
     print("Starting to load term-topic context as fimi to path: ", incidence_save_path)
     doc_topic_ctx = csv2ctx(path_to_file=incidence_save_path, filename=top_doc_filename, prefix="doc_")
-    ctx2fimi(doc_topic_ctx, path_to_file=incidence_save_path, filename=f"doc_topic_fimi_{date}", prefix="doc_")
+    ctx2fimi(doc_topic_ctx, path_to_file=incidence_save_path, filename=f"doc_topic_fimi_{save_date}", prefix="doc_")
     print("Doc-topic context loaded and saved as fimi to path: ", incidence_save_path)
     print("--------------------------")
 
     # Load the term-topic context
     print("Starting to load term-topic context as fimi to path: ", incidence_save_path)
     term_topic_ctx = csv2ctx(path_to_file=incidence_save_path, filename=term_topic_filename, prefix="term_")
-    ctx2fimi(term_topic_ctx, path_to_file=incidence_save_path, filename=f"term_topic_fimi_{date}", prefix="term_")
+    ctx2fimi(term_topic_ctx, path_to_file=incidence_save_path, filename=f"term_topic_fimi_{save_date}", prefix="term_")
     print("Term-topic context loaded and saved as fimi to path: ", incidence_save_path)
 
     # TODO: obtain intents efficiently via pcbo (terminal)
