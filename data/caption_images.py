89b7e97336aec64bb3a2b6555262ab693d00449c
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-15 17:03:13 +0100

    feat: Restructure code to a class

--- data/caption_images.py
+++ data/caption_images.py
@@ -5,25 +5,47 @@ from transformers import AutoProcessor, AutoModelForCausalLM
 from PIL import Image
 
 
-# https://huggingface.co/docs/transformers/main/tasks/image_captioning (15.01.2025)
+class ImageCaptioner:
+
+    def __init__(self):
+        """
+        Initialize the image captioning model.
+        The model is based on the `microsoft/git-base` model.
+        Find supplementary information about the model here:
+        https://huggingface.co/docs/transformers/main/tasks/image_captioning (15.01.2025)
+        """
+        # `use_fast` = `True`: fast version, as soon as available
+        #       (not yet, 15.01.2025: falling back to the slow version)
+        # Legacy behavior is being used: if both images and text are provided,
+        #       the last token (EOS token) of the input_ids and attention_mask tensors will be removed.
+        self.processor = AutoProcessor.from_pretrained("microsoft/git-base",
+                                                       use_fast=True, legacy=False)
+        self.model = AutoModelForCausalLM.from_pretrained("microsoft/git-base")
+
+    def __call__(self, image_path: str) -> str:
+        """
+        Generate a caption for the given image.
+        :param image_path: A path to the image file including the file name and extension.
+        :return: The generated caption for the image.
+        """
+        image = Image.open(image_path).convert("RGB")
+        pixel_values = self.processor(images=image, return_tensors="pt").pixel_values
+
+        # Generate captions
+        generated_ids = self.model.generate(pixel_values=pixel_values, max_length=50)
+        return self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    # TODO: save results to a file
+
+
 if __name__ == '__main__':
 
     # Load the processor and model
-    # `use_fast` is set to `True` but the image processor class does not have a fast version.  Falling back to the slow version.
-    # Legacy behavior is being used. The current behavior will be deprecated in version 5.0.0. In the new behavior, if both images and text are provided, the last token (EOS token) of the input_ids and attention_mask tensors will be removed. To test the new behavior, set `legacy=False`as a processor call argument.
-    processor = AutoProcessor.from_pretrained("microsoft/git-base", use_fast=True, legacy=False)
-    model = AutoModelForCausalLM.from_pretrained("microsoft/git-base")
+    captioner = ImageCaptioner()
 
     # Load and preprocess the image
     l_img_paths = sorted(glob(os.path.join('/Users/klara/Downloads/', "*.png")))
     for image_path in tqdm(l_img_paths):
         print("start image", image_path)
-
-        image = Image.open(image_path).convert("RGB")
-        pixel_values = processor(images=image, return_tensors="pt").pixel_values
-
-        # Generate captions
-        generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
-        caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-
+        caption = captioner(image_path)
         print("Generated Caption:", caption)
