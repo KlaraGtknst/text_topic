c95c92c12ae4eba3e97e08dad082f35a1427ff92
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-16 13:54:34 +0100

    style: Correct current server save path

--- constants.py
+++ constants.py
@@ -4,5 +4,5 @@ TEST_TRAINING_PATH = '/Users/klara/Downloads'   #"/Users/klara/Documents/uni/"#
 SERVER_PATH = '/norgay/bigstore/kgu/data/ETYNTKE'   #Exotic Weapons' /Physics
 SERVER_PATH_TO_PROJECT = '/norgay/bigstore/kgu/dev/text_topic/'
 SAVE_PATH = '/Users/klara/Developer/Uni/WiSe2425/text_topic/results/'
-SERVER_SAVE_PATH = '/norgay/bigstore/kgu/dev/text_topic/results/plots/server_080125'
+SERVER_SAVE_PATH = '/norgay/bigstore/kgu/dev/text_topic/results/plots/'
 SERVER_STATS_PATH = '/norgay/bigstore/kgu/dev/clj_exploration_leaks/results/'
