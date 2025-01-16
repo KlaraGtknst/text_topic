3166f0b929021ea9e1a1a8538813827b4cafb98f
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-16 12:57:02 +0100

    fix: Change local save path

--- constants.py
+++ constants.py
@@ -3,6 +3,6 @@ DB_NAME = "txt_db"
 TEST_TRAINING_PATH = '/Users/klara/Downloads'   #"/Users/klara/Documents/uni/"#
 SERVER_PATH = '/norgay/bigstore/kgu/data/ETYNTKE'   #Exotic Weapons' /Physics
 SERVER_PATH_TO_PROJECT = '/norgay/bigstore/kgu/dev/text_topic/'
-SAVE_PATH = '../results'
+SAVE_PATH = '/Users/klara/Developer/Uni/WiSe2425/text_topic/results/'
 SERVER_SAVE_PATH = '/norgay/bigstore/kgu/dev/text_topic/results/plots/server_080125'
 SERVER_STATS_PATH = '/norgay/bigstore/kgu/dev/clj_exploration_leaks/results/'
