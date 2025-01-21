86cc6c26303ab78beba1b88c479116874bbb09bd
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-22 00:33:53 +0100

    fix: log file name of captioner

--- insert_text_captions.py
+++ insert_text_captions.py
@@ -6,7 +6,7 @@ from utils.os_manipulation import exists_or_create
 
 if __name__ == '__main__':
     on_server = True
-    init_debug_config(log_filename='init_elasticsearch_', on_server=on_server)
+    init_debug_config(log_filename='insert_text_captions_', on_server=on_server)
     es_db = db.ESDatabase()
     es_db.insert_text_related_fields(src_path=constants.Paths.SERVER_DATA_PATH.value)
     logging.info('Finished inserting text related fields: text, embedding, named_entities')
