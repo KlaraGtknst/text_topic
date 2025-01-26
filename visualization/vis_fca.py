a19a2d8994a23440ea64f322cf4fbe5535954e10
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-01-26 18:23:19 +0100

    task: Update path

--- visualization/vis_fca.py
+++ visualization/vis_fca.py
@@ -1,5 +1,4 @@
 import logging
-
 import utils.logging_utils as logging_utils
 import utils.os_manipulation as osm
 from topic.topic_fca import TopicFCA
@@ -30,5 +29,5 @@ if __name__ == "__main__":
     # logging_utils.init_debug_config(log_filename='vis_fca_', on_server=on_server)
     path2csv = "/Users/klara/Developer/Uni/WiSe2425/clj_exploration_leaks/results/fca-dir-concepts/across-dir/"
     save_path = f"/Users/klara/Developer/Uni/WiSe2425/text_topic/results/fca/{date}/"
-    filename_of_csv = "across-dir-incidence-matrix.csv"
+    filename_of_csv = "server-across-dir-incidence-matrix.csv"  # "across-dir-incidence-matrix.csv"
     display_context(path2csv=path2csv, save_path=save_path, filename_of_csv=filename_of_csv)
