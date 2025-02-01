1fcb2e3e5fe0c04d57f665f43acceedd620ff89c
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-02-01 09:54:10 +0100

    task: Try if every lib is installed is present on small dataset

--- eval_topic_modeling.py
+++ eval_topic_modeling.py
@@ -11,7 +11,7 @@ import data.files as files
 import topic.topic_modeling as tm
 
 if __name__ == '__main__':
-    date = datetime.now().strftime('%x').replace('/', '_')
+    date = '01_31_25' #datetime.now().strftime('%x').replace('/', '_')
     on_server = True
     init_debug_config(log_filename='eval_topic_modeling_', on_server=on_server)
     data_path = Paths.SERVER_DATA_PATH.value
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     logging.info('Obtained Elasticsearch client and inserted metadata')
     paths2sentences = {}
     # texts
-    file_paths = files.get_files(path=data_path)
+    file_paths = files.get_files(path=data_path)[:10]   # TODO: remove this later
     logging.info('Obtained pdfs')
     for file_path in tqdm.tqdm(file_paths, desc='Extracting text from pdfs'):
         sentence = files.extract_text_from_pdf(file_path)
@@ -39,24 +39,24 @@ if __name__ == '__main__':
     model.save_model(path=model_path)  # unique name with date
     logging.info('Created and saved topic model')
 
-    # document-topic incidence
-    start = 0
-    duration = len(sentences)
-    doc_ids = list(range(start, start + len(sentences[start:start + duration]) - 1))
-
-    doc_topic_incidence = model.get_document_topic_incidence(doc_ids=doc_ids)
-    doc_topic_save_path = incidence_save_path + f"doc_topic_incidence_{date}.csv"
-    files.save_df_to_csv(df=doc_topic_incidence, path=incidence_save_path, file_name=f"doc_topic_incidence_{date}")
-    logging.info("obtained & saved doc-topic incidence to ", doc_topic_save_path)
+    # # document-topic incidence
+    # start = 0
+    # duration = len(sentences)
+    # doc_ids = list(range(start, start + len(sentences[start:start + duration]) - 1))
+    #
+    # doc_topic_incidence = model.get_document_topic_incidence(doc_ids=doc_ids)
+    # doc_topic_save_path = incidence_save_path + f"doc_topic_incidence_{date}.csv"
+    # files.save_df_to_csv(df=doc_topic_incidence, path=incidence_save_path, file_name=f"doc_topic_incidence_{date}")
+    # logging.info("obtained & saved doc-topic incidence to ", doc_topic_save_path)
 
     # determine optimal threshold for document-topic incidence
-    threshold, row_norm_doc_topic_df = model.determine_threshold_doc_topic_threshold(doc_topic_incidence,
-                                                                                     opt_density=0.1,
-                                                                                     save_path=plot_save_path)
-    logging.info(f"optimal threshold: {threshold}")
-    thres_row_norm_doc_topic_df = model.apply_threshold_doc_topic_incidence(row_norm_doc_topic_df, threshold=threshold)
-    files.save_df_to_csv(df=thres_row_norm_doc_topic_df, path=incidence_save_path,
-                         file_name=f"thres_row_norm_doc_topic_incidence_{date}")
+    # threshold, row_norm_doc_topic_df = model.determine_threshold_doc_topic_threshold(doc_topic_incidence,
+    #                                                                                  opt_density=0.1,
+    #                                                                                  save_path=plot_save_path)
+    # logging.info(f"optimal threshold: {threshold}")
+    # thres_row_norm_doc_topic_df = model.apply_threshold_doc_topic_incidence(row_norm_doc_topic_df, threshold=threshold)
+    # files.save_df_to_csv(df=thres_row_norm_doc_topic_df, path=incidence_save_path,
+    #                      file_name=f"thres_row_norm_doc_topic_incidence_{date}")
     thres_row_norm_doc_topic_save_path = incidence_save_path + f"doc_topic_incidence_{date}.csv"
     # thres_row_norm_doc_topic_df = pd.read_csv(incidence_save_path + f"thres_row_norm_doc_topic_incidence_{date}", index_col=0)
     logging.info("obtained & saved thresholded doc-topic incidence")
@@ -64,9 +64,9 @@ if __name__ == '__main__':
     # test term-topic incidence
     term_topic_save_path = incidence_save_path + f"term_topic_incidence_{date}.csv"
     save_path_topic_words = incidence_save_path + f'topic_words_{date}.json'
-    term_topic_incidence = model.get_term_topic_incidence(doc_ids=doc_ids,
-                                                          save_path_topic_words=save_path_topic_words)
-    files.save_df_to_csv(term_topic_incidence, incidence_save_path, f"term_topic_incidence_{date}")
+    # term_topic_incidence = model.get_term_topic_incidence(doc_ids=doc_ids,
+    #                                                       save_path_topic_words=save_path_topic_words)
+    # files.save_df_to_csv(term_topic_incidence, incidence_save_path, f"term_topic_incidence_{date}")
     logging.info("obtained & saved term-topic incidence in ", term_topic_save_path)
 
     average_precision, average_recall = model.evaluate_topic_model(doc_topic_incidence_path=thres_row_norm_doc_topic_save_path,
